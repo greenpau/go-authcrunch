f5c7d149e5e92fbf4d33c6f47b6a4526a127b150
Paul Greenberg <greenpau@outlook.com>
2022-01-17 08:42:07 -0500

    update license headers

--- config.go
+++ config.go
@@ -1,3 +1,17 @@
+// Copyright 2022 Paul Greenberg greenpau@outlook.com
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package aaasf
 
 import (
@@ -19,4 +33,19 @@ func NewConfig() *Config {
 	return &Config{
 		credMap: make(map[string]*credentials.Config),
 	}
+}
+
+// AddCredential adds a credential configuration.
+func (cfg *Config) AddCredential(m map[string]interface{}) error {
+	return nil
+}
+
+// AddCredential adds an authentication portal configuration.
+func (cfg *Config) AddAuthenticationPortal(m map[string]interface{}) error {
+	return nil
+}
+
+// AddCredential adds an authorization policy configuration.
+func (cfg *Config) AddAuthorizationPolicy(m map[string]interface{}) error {
+	return nil
 }
