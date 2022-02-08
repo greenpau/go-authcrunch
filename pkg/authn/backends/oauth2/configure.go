31b6cfc26e2aeef45ee4b18abe6fd6649aa639b7
Antonio Cunha <aj.20.cunha@gmail.com>
2022-02-08 17:32:13 +0000

    oauth: change http client in fetchMetadataURL to use the client that uses proxy from environment Resolves: greenpau/go-authcrunch#5 Signed-off: Antonio Cunha <aj.20.cunha@gmail.com>

--- pkg/authn/backends/oauth2/configure.go
+++ pkg/authn/backends/oauth2/configure.go
@@ -389,7 +389,12 @@ func (b *Backend) fetchConfig() error {
 }
 
 func (b *Backend) fetchMetadataURL() error {
-	resp, err := http.Get(b.Config.MetadataURL)
+	cli, err := newBrowser()
+	if err != nil {
+		return err
+	}
+	req, err := http.NewRequest("GET", b.Config.MetadataURL, nil)
+	resp, err := cli.Do(req)
 	if err != nil {
 		return err
 	}
