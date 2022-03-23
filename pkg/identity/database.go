8ef3221a880870a24415ca7c35445610d5c29b95
Paul Greenberg <greenpau@outlook.com>
2022-03-23 11:13:39 -0400

    released v1.0.19

--- pkg/identity/database.go
+++ pkg/identity/database.go
@@ -61,9 +61,9 @@ func init() {
 	app = versioned.NewPackageManager("authdb")
 	app.Description = "authdb"
 	app.Documentation = "https://github.com/greenpau/go-authcrunch"
-	app.SetVersion(appVersion, "1.0.18")
+	app.SetVersion(appVersion, "1.0.19")
 	app.SetGitBranch(gitBranch, "main")
-	app.SetGitCommit(gitCommit, "v1.0.18")
+	app.SetGitCommit(gitCommit, "v1.0.18-4-gb0bcd90")
 	app.SetBuildUser(buildUser, "")
 	app.SetBuildDate(buildDate, "")
 }
