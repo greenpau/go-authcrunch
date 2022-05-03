f3ca98c1419a4fcdbe856864541a247f992fe35d
Pedro Flores <pedro.flores@vw-dilab.com>
2022-05-03 09:20:20 +0100

    Add additional validations for additional scopes

--- pkg/util/validate/additional_scopes.go
+++ pkg/util/validate/additional_scopes.go
@@ -7,7 +7,7 @@ import (
 
 // AdditionalScopes verifies if the provided additional_scopes argument is valid
 func AdditionalScopes(additionalScopes string) error {
-	compile, _ := regexp.Compile("[^\\w%20$]+")
+	compile, _ := regexp.Compile("(^[\\S]+$)|([\\w_]%20)")
 	match := compile.MatchString(additionalScopes)
 	if match == false {
 		return errors.ErrInvalidAdditionalScopes
