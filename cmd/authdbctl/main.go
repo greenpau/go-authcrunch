92534c91481c27f736f63f5df8ed52b35640542c
Paul Greenberg <greenpau@outlook.com>
2022-05-07 16:04:32 -0400

    released v1.0.26

--- cmd/authdbctl/main.go
+++ cmd/authdbctl/main.go
@@ -37,9 +37,9 @@ func init() {
 	app = versioned.NewPackageManager("authdbctl")
 	app.Description = "AuthDB management client"
 	app.Documentation = "https://github.com/greenpau/go-authcrunch/"
-	app.SetVersion(appVersion, "1.0.25")
+	app.SetVersion(appVersion, "1.0.26")
 	app.SetGitBranch(gitBranch, "main")
-	app.SetGitCommit(gitCommit, "v1.0.24-1-g6a135a3")
+	app.SetGitCommit(gitCommit, "v1.0.25-4-g9e7b55d")
 	app.SetBuildUser(buildUser, "")
 	app.SetBuildDate(buildDate, "")
 
--- pkg/identity/database.go
+++ pkg/identity/database.go
@@ -61,9 +61,9 @@ func init() {
 	app = versioned.NewPackageManager("authdb")
 	app.Description = "authdb"
 	app.Documentation = "https://github.com/greenpau/go-authcrunch"
-	app.SetVersion(appVersion, "1.0.25")
+	app.SetVersion(appVersion, "1.0.26")
 	app.SetGitBranch(gitBranch, "main")
-	app.SetGitCommit(gitCommit, "v1.0.25")
+	app.SetGitCommit(gitCommit, "v1.0.25-4-g9e7b55d")
 	app.SetBuildUser(buildUser, "")
 	app.SetBuildDate(buildDate, "")
 }
