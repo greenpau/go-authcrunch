5d16ab26cb0904577c6f1a7ba8c74288bcd93c66
Paul Greenberg <greenpau@outlook.com>
2025-03-25 08:41:11 -0400

    fix: adjust api key regex pattern
    See also: github.com/greenpau/caddy-security#382

--- pkg/authn/api_add_user_api_key.go
+++ pkg/authn/api_add_user_api_key.go
@@ -29,7 +29,7 @@ import (
 )
 
 var apiKeyTitleRegexPattern = regexp.MustCompile(`^[\w\@\.\s\(\)]+$`)
-var apiKeyRegexPattern1 = regexp.MustCompile(`^[A-Za-z0-9]{64,72}$`)
+var apiKeyRegexPattern1 = regexp.MustCompile(`^[A-Za-z0-9]{72,96}$`)
 
 // AddUserAPIKey adds API key to user identity.
 func (p *Portal) AddUserAPIKey(
