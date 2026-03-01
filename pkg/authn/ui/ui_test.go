86cc3037acdd4da4e27b355533f3da982e97a85d
Paul Greenberg <greenpau@outlook.com>
2026-03-01 17:27:00 -0500

    bugfix: update handling of templates

--- pkg/authn/ui/ui_test.go
+++ pkg/authn/ui/ui_test.go
@@ -18,6 +18,8 @@ import (
 	"bytes"
 	"strings"
 	"testing"
+
+	"github.com/greenpau/go-authcrunch/pkg/translate"
 )
 
 func TestNewFactory(t *testing.T) {
@@ -146,5 +148,148 @@ func TestAddBuiltinTemplates(t *testing.T) {
 	t.Logf("Re-adding templates")
 	if err := f.AddBuiltinTemplate("basic/portal"); err != nil {
 		t.Fatalf("Expected success, but got error: %s", err)
+	}
+}
+
+func TestRenderTemplate(t *testing.T) {
+	// Define the table structure for template rendering tests
+	var testcases = []struct {
+		name      string
+		template  string
+		disabled  bool
+		pageTitle string
+		data      map[string]interface{}
+		shouldErr bool
+	}{
+		{
+			name:     "render portal template",
+			template: "basic/portal",
+			data: map[string]interface{}{
+				"title": "My Portal",
+			},
+		},
+		{
+			name:     "render mobile access template",
+			template: "basic/apps_mobile_access",
+		},
+		{
+			name:      "render login template",
+			template:  "basic/login",
+			pageTitle: translate.Translate("sign_in", translate.English, nil),
+			data: map[string]interface{}{
+				"authenticated": false,
+				"login_options": map[string]interface{}{
+					"authenticators": []interface{}{
+						map[string]interface{}{
+							"background_color":     "#324960",
+							"class_name":           "las la-key la-2x",
+							"color":                "white",
+							"realm":                "local",
+							"registration_enabled": "yes",
+							"text":                 "LOCAL",
+							"text_color":           "#37474f",
+						},
+					},
+					"authenticators_required":   "no",
+					"default_realm":             "local",
+					"form_required":             "yes",
+					"hide_contact_support_link": "yes",
+					"hide_forgot_username_link": "yes",
+					"hide_links":                "no",
+					"hide_register_link":        "no",
+					"identity_required":         "yes",
+					"realm_dropdown_required":   "no",
+					"realms": []interface{}{
+						map[string]interface{}{
+							"default": "yes",
+							"label":   "LOCAL",
+							"realm":   "local",
+						},
+					},
+				},
+				"i18n_provide_username_or_email": translate.Translate("provide_username_or_email", translate.English, nil),
+				"i18n_back_action":               translate.Translate("back_action", translate.English, nil),
+				"i18n_proceed_action":            translate.Translate("proceed_action", translate.English, nil),
+				"i18n_register_action":           translate.Translate("register_action", translate.English, nil),
+				"i18n_forgot_username_question":  translate.Translate("forgot_username_question", translate.English, nil),
+				"i18n_contact_support_action":    translate.Translate("contact_support_action", translate.English, nil),
+			},
+		},
+		{
+			name:     "render generic template",
+			template: "basic/generic",
+			data:     map[string]interface{}{},
+		},
+		{
+			name:     "render sandbox template",
+			template: "basic/sandbox",
+		},
+		{
+			name:     "render registration template",
+			template: "basic/register",
+			data:     map[string]interface{}{},
+		},
+		{
+			name:      "render sso applications template",
+			template:  "basic/apps_sso",
+			pageTitle: "AWS SSO",
+			data: map[string]interface{}{
+				"role_count": 1,
+				"roles": []interface{}{
+					map[string]interface{}{
+						"Name":         "foo",
+						"AccountID":    "1234567890",
+						"ProviderName": "aws",
+					},
+				},
+			},
+		},
+		{
+			name:      "render whoami template",
+			template:  "basic/whoami",
+			pageTitle: translate.Translate("user_identity_heading", translate.English, nil),
+			data: map[string]interface{}{
+				"token":             `{"authenticated": true}`,
+				"i18n_portal_label": translate.Translate("portal_label", translate.English, nil),
+				"i18n_sign_out":     translate.Translate("sign_out", translate.English, nil),
+			},
+		},
+	}
+
+	f := NewFactory()
+	if err := f.AddBuiltinTemplates(); err != nil {
+		t.Fatalf("failed to load builtin templates: %v", err)
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			if tc.disabled {
+				t.Skip("Test disabled")
+				return
+			}
+
+			args := f.GetArgs()
+
+			args.PageTitle = tc.pageTitle
+
+			for k, v := range tc.data {
+				args.Data[k] = v
+			}
+
+			output, err := f.Render(tc.template, args)
+
+			if (err != nil) != tc.shouldErr {
+				t.Errorf("Render() error = %v, wantErr %v", err, tc.shouldErr)
+				return
+			}
+
+			if !tc.shouldErr {
+				if output == nil {
+					t.Errorf("Render() returned nil buffer")
+				} else if output.Len() == 0 {
+					t.Errorf("Render() returned empty output for template %s", tc.template)
+				}
+			}
+		})
 	}
 }
